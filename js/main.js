91d95aca51b96ec50a955b6d5a8a8753164cfd66
bryanbill <emobrya@gmail.com>
2022-03-13 20:10:46 +0300

    feat:mail - added mailchimp functionality

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function ($) {
+    window.fnames = new Array();
+    window.ftypes = new Array();
+    fnames[1] = "USERNAME";
+    ftypes[1] = "text";
+    fnames[0] = "EMAIL";
+    ftypes[0] = "email";
+    fnames[2] = "MESSAGE";
+    ftypes[2] = "text";
+})(jQuery);
+var $mcj = jQuery.noConflict(true);
